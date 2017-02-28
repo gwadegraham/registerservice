9bcc32c764007008e9d549721cc4a8381af763fc
Pedro Rodriguez <pr003@uark.edu>
2017-02-27 22:42:47 -0600

    Created EmployeeRestController

---
+++ src/main/java/edu/uark/controllers/EmployeeRestController.java
@@ -0,0 +1,15 @@
+package edu.uark.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by pedro on 2/27/17.
+ */
+
+@RestController
+@RequestMapping(value = "/employee")
+public class EmployeeRestController {
+
+
+}
