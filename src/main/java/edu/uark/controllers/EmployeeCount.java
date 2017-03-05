0198a944d6d506bc2bbe3f2a8b46563f2b2d5dd3
SangYun <SangYun@Macs-MacBook-Pro.local>
2017-03-05 12:34:28 -0600

    sangyun

---
+++ src/main/java/edu/uark/controllers/EmployeeCount.java
@@ -0,0 +1,14 @@
+package edu.uark.controllers;
+
+public class EmployeeCount {
+	private int countEmployee;
+	
+	public void setCount(int count) {
+		this.countEmployee = count;
+	}
+	
+	public int getCount() {
+		return countEmployee;
+	}
+
+}
