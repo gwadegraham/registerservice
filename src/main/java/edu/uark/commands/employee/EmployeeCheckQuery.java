6b7ae5d38a77412d6dea3619cd2961412a93a74f
SangYun <SangYun@Macs-MacBook-Pro.local>
2017-03-06 13:31:40 -0600

    sangyun

---
+++ src/main/java/edu/uark/commands/employee/EmployeeCheckQuery.java
@@ -0,0 +1,15 @@
+package edu.uark.commands.employee;
+
+import edu.uark.commands.ResultCommandInterface;
+import edu.uark.models.api.Employee;
+
+public class EmployeeCheckQuery implements ResultCommandInterface<Employee>{
+
+	@Override
+	public Employee execute() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+
+}
